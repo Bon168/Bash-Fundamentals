675927012aabd8b1d7ebd9d6f4855ef65c640bd3
chinae <Chinaenwankwo@gmail.com>
2026-04-30 10:32:55 -0500

    first part

---
+++ What_is_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+ Bash Fundamentals Repository
+
+What is Bash?
+Bash is a command-line tool used in Linux to run commands, create scripts, and automate tasks.
+
+
+
+How This Repo Helps Me
+This repo helps me practice Bash by focusing on one concept per file. It builds my skills and gives me a place to review what I’ve learned.
+
