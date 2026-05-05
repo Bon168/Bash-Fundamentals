9c34c845587833ed72a3e7e3aea983def401a25d
chinae <Chinaenwankwo@gmail.com>
2026-05-05 10:28:10 -0500

    Next part

--- What_is_bash.sh
+++ What_is_bash.sh
@@ -11,3 +11,11 @@ Bash is a command-line tool used in Linux to run commands, create scripts, and a
 How This Repo Helps Me
 This repo helps me practice Bash by focusing on one concept per file. It builds my skills and gives me a place to review what I’ve learned.
 
+Why Documentation Matters
+This helps me and others understand the code, debug any problems that may arise, and have good knowlege of the code at hand.
+
+
+Purpose of Git
+Git helps to track changes, will save process, and allows you to go to older versions of your code and fix mistakes
+
+
