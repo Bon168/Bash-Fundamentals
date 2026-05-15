8060a1ba582251a8aa52c40d556e0e93dab48f0d
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-15 10:15:54 -0500

    JUST DID THE TWO-d arrays

---
+++ Two-dimensional_arrays.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#most common way of using a 2D array is to use an asssociative array 
+#where the key is a combination of the row and column shows
+
+
+#Using this is to declare an associative array
+declare -A matrix
+
+#next you assign the values
+
+matrix[0,0]="Row 0, Col 0"
+matrix[0,1]="Row 0, Col 1"
+matrix[1,0]="Row 1, Col 0"
+
+#TO USE THE VALUE
+
+echo "${matrix[0,1]}"
