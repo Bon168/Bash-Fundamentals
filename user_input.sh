ec425466686993eded38f2715e52b09c2e4bba21
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-20 09:51:55 -0500

    ADDING NOTES I MISSED

--- user_input.sh
+++ user_input.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#This would be a example of a user input, where you give the user a text
+#And they must answer then the system will pop out there name
 echo "what is your name"
 read name 
 
