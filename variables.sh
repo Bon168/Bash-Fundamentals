bf803bb9e78c8b54fa2222fd79d53714c6294066
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-20 09:50:05 -0500

    ADDING NOTES I MISSED

--- variables.sh
+++ variables.sh
@@ -2,8 +2,9 @@
 
 name="Chinae"
 age=16
-
+#This would be a example of a variable that I created 
 
 echo "My name is $name"
 echo "I am $age years old"
 
+#Then this would be you using the variables
