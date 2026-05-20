902b4dc9346fb739a4b45917cb816e2828b50aa8
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-20 09:56:56 -0500

    ADDING NOTES I MISSED

--- printf.sh
+++ printf.sh
@@ -3,3 +3,7 @@
 name="Fran"
 printf "Hello, %s\n" "$name"
 printf "Number: %d\n" 10
+#provides a format string followed by arguments
+# %s This will treat the argument as a string
+# %d Treates the arguments as a signed decimal integer
+#f treats the arguments as a floating-point number
