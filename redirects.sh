20d54c1d9df7e2a0d060e3ecc0d2dbfae3af9c5e
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-21 10:05:14 -0500

    ANYTHING NEW

--- redirects.sh
+++ redirects.sh
@@ -7,3 +7,9 @@ echo "Hello world" > file.txt
 #adds on instead of just overwrite
 
 echo "Earth" >> file.txt
+
+# Common Mistake:
+# Over writes the code
+
+# Trouble shooting
+# Use command 2> error.log?
