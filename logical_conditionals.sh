e708780c94287e077a22a4ea3856ead326a46eb6
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-21 10:13:02 -0500

    ANYTHING NEW

--- logical_conditionals.sh
+++ logical_conditionals.sh
@@ -6,7 +6,11 @@ if [ $num -gt 5 ] && [ $num -lt 10 ]; then
   echo "Between 5 and 10"
 fi
 
+#How it works:
 #logical conditionals are mainly used with if statements
 # A base logical conditional needs a condition closed with square brackets
 #There are alot of common operators that can be used
 #This one is -gt, -lt This is just greater than, and less than
+
+#Common mistakes:
+#Not using the correct brackets
