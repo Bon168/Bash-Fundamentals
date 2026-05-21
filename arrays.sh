a8f0ddce87932292c52d4273a40f21d89bf23d1a
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-21 10:11:46 -0500

    ANYTHING NEW

--- arrays.sh
+++ arrays.sh
@@ -3,4 +3,11 @@ my_array=("cat" "dog" "lion" "coin")
 
 echo "my_array"
 
+#How it works:
+#you type a something put a = then add brackets ("") and it will read out what is in your array
+
+#Common mistakes:
+#Not putting "" on your code
+
+#Troubles
 #Very simple array example
