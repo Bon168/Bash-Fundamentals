ef45f4ae853c60e0384c7716ecf24383e92be90c
chinae <197675881+Bon168@users.noreply.github.com>
2026-05-21 10:15:56 -0500

    ANYTHING NEW

--- printf.sh
+++ printf.sh
@@ -3,7 +3,17 @@
 name="Fran"
 printf "Hello, %s\n" "$name"
 printf "Number: %d\n" 10
+
+#How it works:
 #provides a format string followed by arguments
 # %s This will treat the argument as a string
 # %d Treates the arguments as a signed decimal integer
 #f treats the arguments as a floating-point number
+
+#Common mistakes:
+#Using the wrong specifiers, make sure you are using the correct one
+#%s: Treats the argument as a string.
+#%d: Treats the argument as a signed decimal integer.
+#%f: Treats the argument as a floating-point number.
+#%x: Converts an integer to lowercase hexadecimal.
+#%%: Prints a literal percent sign. 
